60dc1711383108e4f3c6e052304f2017a0af7f01
violetyy0305 <137112254+violetyy0305@users.noreply.github.com>
2023-08-26 16:26:02 +0000

    stored data to db

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_violet():
   jobs = load_jobs_from_db()
+
   return render_template("home.html", jobs=jobs)
 
 
@@ -15,6 +16,27 @@ def hello_violet():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template("jobpage.html", job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  # store this in the DB
+  add_application_to_db(id, data)
+  # dispplay an acknowledge
+  # send an email
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, text
-import os
+#import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+#db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = "mysql+pymysql://oz86mmb94z2zy9o6tlia:pscale_pw_R8IL4f2NSNKUaESeCeGP2bACuLioJaEUOx7k86lYwfH@aws.connect.psdb.cloud/mindgenomics?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
@@ -22,3 +23,29 @@ def load_jobs_from_db():
         d[keys[i]] = job[i]
       jobs.append(d)
   return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id=:val"),
+                          {"val": id})
+    row = result.all()
+    if len(row) == 0:
+      return None
+    else:
+      keys = list(result.keys())
+      d = {}
+      for i in range(len(row[0])):
+        d[keys[i]] = row[0][i]
+      return d
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query=text("insert into applications(job_id, full_name, email, education) values (:job_id, :full_name, :email, :education)")
+
+    conn.execute(query,
+                {"job_id":job_id,
+                "full_name":data['full_name'],
+                "email":data['email'],
+                "education":data['education']})
+    
