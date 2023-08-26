7c0a30bde5daebf0b14b79708fdf0835456699f4
violetyy0305 <137112254+violetyy0305@users.noreply.github.com>
2023-08-26 14:29:09 +0000

    added secrete db_connection string

--- database.py
+++ database.py
@@ -1,21 +1,12 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connection_string = "mysql+pymysql://ekpm9fcfuyubgabwu1nt:pscale_pw_sHjTCvTJOLiqzEvqhO1noa7taqDEilVXvJ7M1MRF03H@aws.connect.psdb.cloud/mindgenomics?charset=utf8mb4"
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
-'''
-def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-
-    jobs = []
-    for row in result.all():
-      jobs.append(dict(row))
-    return jobs
-'''
 
 
 def load_jobs_from_db():
